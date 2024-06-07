3d0a481d0f79d20108ff868d4b81a040429f0687
cdufour <opusidea@gmail.com>
2024-06-07 14:01:09 +0200

    tp final

--- demo/src/app/ajc/student-form/student-form.component.ts
+++ demo/src/app/ajc/student-form/student-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Student } from '../student';
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-student-form',
@@ -10,6 +11,8 @@ export class StudentFormComponent {
   studentName = "Kevin";
   student: Student = { name: 'Chris', grade: 10};
 
+  constructor(private studentService: StudentService) {}
+
   onChange(event: any) {
     console.log(event.target.value);
     this.studentName = event.target.value;
@@ -19,5 +22,8 @@ export class StudentFormComponent {
     console.log('form submitted...');
 
     // requête http (ajax) ici
+    this.studentService
+      .postStudent(this.student)
+      .subscribe(student => console.log(student))
   }
 }
